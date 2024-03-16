a6daddafd8c945e69df7347a951d68adced7e1bf
dhritisingh18 <dhritisingh3@gmail.com>
2024-03-16 16:34:02 -0400

    Updates

---
+++ precache-manifest.b1e8dfab82e72dc188e719c1bee2b056.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46c26fde5c1fc90e8c837cce0eef1b1",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "d2dc5f37064ce8a53e44",
+    "url": "/movies/static/css/main.c2b42bbc.chunk.css"
+  },
+  {
+    "revision": "8deaa764be1aea14746b",
+    "url": "/movies/static/js/2.a192e5d5.chunk.js"
+  },
+  {
+    "revision": "f0ce071822772ea2c1310c56ac76eec8",
+    "url": "/movies/static/js/2.a192e5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca55376e820242e6cb1",
+    "url": "/movies/static/js/3.5f83b0ef.chunk.js"
+  },
+  {
+    "revision": "d2dc5f37064ce8a53e44",
+    "url": "/movies/static/js/main.f7469a76.chunk.js"
+  },
+  {
+    "revision": "29cc52a8e2440c48e150",
+    "url": "/movies/static/js/runtime-main.c7694f6c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/movies/static/media/logo.06e73328.svg"
+  }
+]);
